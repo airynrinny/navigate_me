9d2959850c737209f962204839b076e1cd417f0a
06NA <129022434+06NA@users.noreply.github.com>
2023-12-11 07:39:44 +0800

    Create homepage.dart

---
+++ lib/homepage.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'pickDest.dart';
+import 'adminHome.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(
+              "images/Homepage.png",
+              fit: BoxFit.cover,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Navigate Me',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  Image.asset(
+                    'images/iium_logo.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                  const SizedBox(height: 16.0),
+                  const Text(
+                    'Welcome to IIUM Gombak Campus Map!',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  const Text(
+                    'Guest: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Button Pressed - Navigating to PickDest");
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => PickDest(),
+                      ));
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                    ),
+                    child: const Text('Start Searching', style: TextStyle(color: Colors.black)),
+                  ),
+                  const SizedBox(height: 16.0),
+                   const Text(
+                    'Admin: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Button Pressed - Sign in for Admin");
+
+                      // Navigate to the adminHome.dart page using the navigator from MaterialApp
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => AdminHomePage()));
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                    ),
+                    child: const Text('Sign in for Admin', style: TextStyle(color: Colors.black)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

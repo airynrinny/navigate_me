63958c1905e3d3ae547b89c4c371ee18a41daf08
airynrinny <airinfaqihah@gmail.com>
2023-12-16 17:37:03 +0800

    added the imgages for startingPoint

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:navigate_me/main.dart';
+import 'package:navigate_me/startingPoint.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

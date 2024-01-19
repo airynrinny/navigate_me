5c57e52a1b97d0f3e3dac1473cc8fb11cb4b671e
06NA <129022434+06NA@users.noreply.github.com>
2024-01-19 22:26:16 +0800

    Update naviGuideCafe_DCS.dart

--- lib/naviGuideCafe_DCS.dart
+++ lib/naviGuideCafe_DCS.dart
@@ -29,7 +29,11 @@ class NaviGuideCafe_DCSPage extends StatelessWidget {
                       ),
                       const Text(
                         'Starting point: Cafe',
-                        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const Text(
+                        'Starting point: Department of Computer Science',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                       ),
                       const SizedBox(height: 40.0),
                       _buildStep('assets/images/Cafelv4/1DCS.png', 'STEP 1: KICT Cafeteria', 'Start at cafe'),

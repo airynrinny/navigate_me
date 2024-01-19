f85e8c922af6ea43bed3bb9fe0cba60992e3e57e
06NA <129022434+06NA@users.noreply.github.com>
2024-01-19 22:27:47 +0800

    Update naviGuideD_CyberSec.dart

--- lib/naviGuideD_CyberSec.dart
+++ lib/naviGuideD_CyberSec.dart
@@ -31,11 +31,16 @@ class NaviGuideD_CyberSecPage extends StatelessWidget {
                       const Text(
                         'Starting point: Block D',
                         style: TextStyle(
-                            fontSize: 28, fontWeight: FontWeight.bold),
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const Text(
+                        'Starting point: CyberSecurity',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
                       ),
                       const SizedBox(height: 40.0),
                       ListTile(
-                        leading: Image.asset('images/blockd-cyber/1.png'),
+                        leading: Image.asset('assets/images/blockd-cyber/1.png'),
                         title: const Text(
                             'STEP 1: Take the elevator and go to Level 3'),
                         subtitle:
@@ -43,13 +48,13 @@ class NaviGuideD_CyberSecPage extends StatelessWidget {
                       ),
                       ListTile(
                         leading: Image.asset(
-                            'images/blockd-cyber/2.png'), // Replace 'step2.jpg' with your actual image path
+                            'assets/images/blockd-cyber/2.png'), // Replace 'step2.jpg' with your actual image path
                         title: const Text('STEP 2: Turn right'),
                         subtitle: const Text('Turn right'),
                       ),
                       ListTile(
                         leading: Image.asset(
-                            'images/blockd-cyber/3.png'), // Replace 'step3.jpg' with your actual image path
+                            'assets/images/blockd-cyber/3.png'), // Replace 'step3.jpg' with your actual image path
                         title: const Text(
                             'STEP 3: Go straight until you reach the corner'),
                         subtitle: const Text(
@@ -57,32 +62,32 @@ class NaviGuideD_CyberSecPage extends StatelessWidget {
                       ),
                       ListTile(
                         leading: Image.asset(
-                            'images/blockd-cyber/4.png'), // Replace 'step4.jpg' with your actual image path
+                            'assets/images/blockd-cyber/4.png'), // Replace 'step4.jpg' with your actual image path
                         title: const Text('STEP 4: At the corner, turn right'),
                         subtitle: const Text('At the corner, turn right'),
                       ),
                       ListTile(
                         leading: Image.asset(
-                            'images/blockd-cyber/5.png'), // Replace 'step4.jpg' with your actual image path
+                            'assets/images/blockd-cyber/5.png'), // Replace 'step4.jpg' with your actual image path
                         title: const Text('STEP 5: Go straight'),
                         subtitle: const Text('Go straight'),
                       ),
                       ListTile(
                         leading: Image.asset(
-                            'images/blockd-cyber/6.png'), // Replace 'step4.jpg' with your actual image path
+                            'assets/images/blockd-cyber/6.png'), // Replace 'step4.jpg' with your actual image path
                         title: const Text('STEP 6: Turn right'),
                         subtitle: const Text('Turn right'),
                       ),
                       ListTile(
                         leading: Image.asset(
-                            'images/blockd-cyber/7.png'), // Replace 'step4.jpg' with your actual image path
+                            'assets/images/blockd-cyber/7.png'), // Replace 'step4.jpg' with your actual image path
                         title: const Text(
                             'STEP 7: Turn left before the fire extinguisher'),
                         subtitle: const Text('You have arrived'),
                       ),
                       ListTile(
                         leading: Image.asset(
-                            'images/blockd-cyber/8.png'), // Replace 'step4.jpg' with your actual image path
+                            'assets/images/blockd-cyber/8.png'), // Replace 'step4.jpg' with your actual image path
                         title: const Text('You have arrived!'),
                         subtitle: const Text('explanation to go from here'),
                       ),
